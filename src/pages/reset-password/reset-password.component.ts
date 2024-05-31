3b4bb9ac6e4aa9382ae57f9f83c76c182de27470
Gustavo Ferreira da Silva <gustavofsilvas@gmail.com>
2024-05-31 16:07:17 -0300

    Defining pages

---
+++ src/pages/reset-password/reset-password.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-reset-password',
+  standalone: true,
+  imports: [],
+  templateUrl: './reset-password.component.html',
+  styleUrl: './reset-password.component.scss'
+})
+export class ResetPasswordComponent {
+
+}
---
+++ src/pages/support/support.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-support',
+  standalone: true,
+  imports: [],
+  templateUrl: './support.component.html',
+  styleUrl: './support.component.scss'
+})
+export class SupportComponent {
+
+}
