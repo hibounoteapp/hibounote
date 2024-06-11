9254d6d98127a8790df5ed08e0a2401b5cae95b8
Gustavo Ferreira da Silva <gustavofsilvas@gmail.com>
2024-06-10 21:50:54 -0300

    🚧Board config menu
    + Mindlab -> Hibounote

--- src/app/features/board/components/navbar/navbar.component.ts
+++ src/app/features/board/components/navbar/navbar.component.ts
@@ -1,29 +1,53 @@
-import { Component, Renderer2 } from '@angular/core';
+import { AfterViewInit, Component, OnInit, Renderer2 } from '@angular/core';
 import { BoardService } from '../../../../shared/services/board/board.service';
 import { BoardDataService } from '../../../../shared/services/board-data/board-data.service';
 import { ActivatedRoute, Router, RouterModule } from '@angular/router';
 import { MatIconModule } from '@angular/material/icon';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'navbar-component',
   standalone: true,
-  imports: [RouterModule, MatIconModule],
+  imports: [RouterModule, MatIconModule, MatButtonModule, MatMenuModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
+
+
 
   constructor(
     public boardService: BoardService,
     public boardData: BoardDataService,
     public activeRoute: ActivatedRoute,
     private renderer: Renderer2,
+    private activatedRoute: ActivatedRoute,
     private router: Router,
   ) {
+
+  }
+
+  changeName(event: Event) {
+    if(event.target instanceof HTMLInputElement) {
+      const value = event.target.value
+      this.boardData.editBoardName(this.boardData.activeId,value)
+    }
+
+  }
+
+  deleteBoard() {
+    //!Confirmation
+    this.boardData.deleteBoard(this.boardData.activeId)
+    this.router.navigate(['/account'])
   }
 
   saveData() {
     this.boardData.saveData();
     this.router.navigate(['/account'])
   }
+
+
 }
--- src/app/features/board/pages/board/board.component.ts
+++ src/app/features/board/pages/board/board.component.ts
@@ -259,6 +259,7 @@ export class BoardComponent implements AfterViewInit, OnInit{
     this.initEvents();
     this.boardService.disablePanzoom()
     this.boardService.enablePanzoom()
+    
   }
 
 }
