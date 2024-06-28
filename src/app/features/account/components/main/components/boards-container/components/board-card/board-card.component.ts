fdc8dde5dbb9441d965ebbb8e1c2c27d4217c616
Gustavo Ferreira da Silva <gustavofsilvas@gmail.com>
2024-06-27 22:10:27 -0300

    Simple board manipulation

--- src/app/features/account/components/main/components/boards-container/components/board-card/board-card.component.ts
+++ src/app/features/account/components/main/components/boards-container/components/board-card/board-card.component.ts
@@ -1,17 +1,19 @@
 import { CommonModule } from '@angular/common';
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { AfterViewInit, Component, EventEmitter, Input, Output, Renderer2 } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 import { Board } from '@custom-interfaces/board';
 import { EditBoardModalComponent } from '../../../../../edit-board-modal/edit-board-modal.component';
 import { MatDialog } from '@angular/material/dialog';
 import { BoardDataService } from '@shared-services/board-data/board-data.service';
 import { IconService } from '@shared-services/icon/icon.service';
+import { MatMenu, MatMenuModule, MatMenuTrigger } from '@angular/material/menu';
+import { DeleteConfirmationComponent } from '../../../../../edit-board-modal/components/delete-confirmation/delete-confirmation.component';
 
 @Component({
   selector: 'board-container-card',
   standalone: true,
-  imports: [RouterModule, MatIconModule, CommonModule],
+  imports: [RouterModule, MatIconModule, CommonModule, MatMenuModule],
   templateUrl: './board-card.component.html',
   styleUrl: './board-card.component.scss'
 })
@@ -23,9 +25,38 @@ export class BoardCardComponent {
   constructor(
     private dialog: MatDialog,
     private boardData: BoardDataService,
-    public iconService: IconService
+    public iconService: IconService,
+    protected renderer: Renderer2,
+    protected router: Router
   ) {
 
+  }
+
+  confirmDelete(id: string) {
+    const dialog = this.dialog.open(DeleteConfirmationComponent)
+
+    dialog.afterClosed().subscribe((result)=>{
+      if(result==="DELETE") {
+        this.boardData.deleteBoard(id);
+        return
+      }
+    })
+  }
+
+  enterBoard(event: Event) {
+    event.preventDefault()
+    if(!(event instanceof MouseEvent)) return
+
+    console.log('ENTERING',event)
+
+    this.router.navigate(['/board'],{
+      queryParams: {id: this.item.id}
+    })
+
+  }
+
+  favBoard(event: Event) {
+    console.log('FAV',event)
   }
 
   // editBoard(event: Event, id: string) {
